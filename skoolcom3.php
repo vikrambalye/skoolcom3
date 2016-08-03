aa7ea377e8c55b107105f43b5f00608a22edc3b1
budumurukrishna <budumurukrishna@gmail.com>
2016-08-03 17:57:38 +0530

    create skoolcom3.php
    skoolcom3 text

---
+++ skoolcom3.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Welcome skoolcom3</title>
+</head>
+<body>
+<form method="post" action="" enctype="multipart/form-data">
+<table>
+<tr>
+<td>Username</td>
+<td><input type="text" name="username"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" name="password"></td>
+</tr>
+<tr>
+<td>Email</td>
+<td><input type="text" name="email"></td>
+</tr>
+<tr>
+<td><input type="submit" value="Submit" name="submit"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
